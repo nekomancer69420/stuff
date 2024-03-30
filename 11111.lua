d90bcc78cf8b4aa73e55726dce5d243776807028
nekoware <87574009+nekomancer69420@users.noreply.github.com>
2024-03-31 00:45:52 +0800

    Create 11111.lua

---
+++ 11111.lua
@@ -0,0 +1,14 @@
+
+Are you a LuaU maestro? Want to put your skills to the test?
+The following are 2 print statements, with each having 16 "elements".
+print(#{2, debug.info(2, "C"), nil, nil, 999, nil, nil, 5, tostring(5), 999, 2, "hi", nil, function(abc, aa) return "free krampsz key plz" end, czzz==b, nil})
+print(#{"abc", 1, warn, 423, nil, setmetatable({}, {__index = function() end}), nil, 5, nil, nil, nil, g==f, a==bne, function(r, re, Q) end, nil, nil})
+Outputs are:
+1 12
+2 4
+
+Now, why do they each not print 16 and why do they differ so much even though they both have 16 "elements"?
+
+
+
+dm me with your answer pls if its correct ill give u a free month of nitro ❤
